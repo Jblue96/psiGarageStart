c2e6a7ac1f1db87319ce2da71482e48ef1375b76
devjanaprime <dev@primeacademy.io>
2017-04-25 22:47:19 -0500

    basic create and read working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,23 @@
 $( document ).ready( function(){
-  console.log( 'JQ' );
+  $( '#addCarButton' ).on( 'click', function(){
+      console.log( 'in addCarButton on click');
+      var objectToSend = {
+        year: $( '#yearIn' ).val(),
+        make: $( '#makeIn' ).val(),
+        model: $( '#modelIn' ).val()
+      }; //end objectToSend
+      console.log( 'sending:', objectToSend );
+      $.ajax({
+        url: '/cars',
+        type: 'POST',
+        data: objectToSend,
+        success: function( response ){
+          console.log( 'back from server with:', response );
+          getCars();
+        } // end success
+      }); //end ajax
+  }); // end addCarButton on click
+
   getCars();
 });
 
@@ -11,6 +29,16 @@ function getCars(){
     success: function( response ){
       console.log( 'back from server with:', response );
       // update dom
+      showCars( response );
     } // end success
   }) // end ajax
 } // end getCars
+
+function showCars( carsArray ){
+  console.log( 'in showCars' );
+  var outputDiv = $( '#outputDiv' );
+  outputDiv.empty();
+  for (var i = 0; i < carsArray.length; i++) {
+    outputDiv.append( '<p>' + carsArray[i].year + ' ' + carsArray[i].make + ' ' + carsArray[i].model + '</p>' );
+  } //end for
+} //end showCars
--- server.js
+++ server.js
@@ -3,6 +3,15 @@ var express = require( 'express' );
 var app = express();
 var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
+var pg = require( 'pg' );
+var config = {
+  database: 'garage',
+  host: 'localhost',
+  port: 5432,
+  max: 10
+};// end config
+
+var pool = new pg.Pool( config );
 
 // uses
 app.use( bodyParser.urlencoded( { extended: true } ) );
@@ -19,15 +28,41 @@ app.get( '/', function ( req, res ){
 
 app.get( '/cars', function( req, res ){
   console.log( 'in /cars get request' );
-  // temp array
-  var cars = [ 'car1', 'car2', 'car3' ];
   // connect to DB
-  // send query to DB that return all cars
-  // translate the resultSet of query to the "cars" array
-  // after the last row has been pushed send back the object with array
-  var objectToSend={
-    status: 200,
-    allCars: cars
-  }; // end objectToSend
-  res.send( objectToSend );
+  pool.connect( function(err, connection, done ){
+    if( err ){
+      console.log( err );
+      res.send( 400 )
+    } // end error
+    else{
+      console.log( 'connected to db' );
+      // send query to DB that return all cars
+      var resultSet = connection.query( "SELECT * from cars" );
+      var carsArray = [];
+      // translate the resultSet of query to the "cars" array
+      resultSet.on( 'row', function( row ){
+        carsArray.push( row );
+      }); //end on row
+      resultSet.on( 'end', function(){
+        // after the last row has been pushed send back the object with array
+        done();
+        res.send( carsArray );
+      }); //end
+    } // end no error
+  }); // end pool connect
 }); //end cars get
+
+app.post( '/cars', function( req, res ){
+  console.log( 'in /cars post:', req.body );
+  pool.connect( function( err, connection, done ){
+    if( err ){
+      console.log( err );
+      res.send( 400 );
+    }// end error
+    else{
+      connection.query( 'INSERT into cars(year, make, model) VALUES( $1, $2, $3 )', [ req.body.year, req.body.make, req.body.model ] );
+      done();
+      res.send( 200 );
+    } // ed no error
+  }); //end pool connect
+}); //end /cars post
